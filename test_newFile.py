2c2e29a55c6ea86891732253fa2005581013d2e1
Scirpo-eng <NikitaSW@inbox.ru>
2026-05-17 12:19:30 +0300

    Fix test

--- test_newFile.py
+++ test_newFile.py
@@ -4,7 +4,7 @@ from newTrainFile import add, get_system_status
 class TestCalculatorAndSystem(unittest.TestCase):
 
     def test_add_strings(self):
-        self.assertEqual(add("Hello", "World"), "Error Text")
+        self.assertEqual(add("Hello", "World"), "HelloWorld")
 
     def test_get_system_status(self):
         status = get_system_status()
